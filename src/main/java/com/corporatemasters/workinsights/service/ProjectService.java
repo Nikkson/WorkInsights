23fd1e31592ec4b9ea389c1aa45e2ed235a83a54
Nikkson <nicky.iliev@abv.bg>
2023-12-19 23:06:50 +0200

    Add findById method in project service

--- src/main/java/com/corporatemasters/workinsights/service/ProjectService.java
+++ src/main/java/com/corporatemasters/workinsights/service/ProjectService.java
@@ -14,6 +14,11 @@ public class ProjectService {
     public List<Project> findAll() {
         return projectRepository.findAll();
     }
+    public Project findById(Long id) {
+        return projectRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid ID: " + id));
+    }
     public void save(Project project) {
         projectRepository.save(project);
     }
