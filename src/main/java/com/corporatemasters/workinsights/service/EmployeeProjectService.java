5303adf02283360fa032927b433a4fe9527e7302
Nikkson <nicky.iliev@abv.bg>
2023-12-22 18:09:56 +0200

    Add validation for new entries in service

--- src/main/java/com/corporatemasters/workinsights/service/EmployeeProjectService.java
+++ src/main/java/com/corporatemasters/workinsights/service/EmployeeProjectService.java
@@ -2,6 +2,8 @@ package com.corporatemasters.workinsights.service;
 
 import com.corporatemasters.workinsights.dto.EmployeeProjectDTO;
 import com.corporatemasters.workinsights.repository.EmployeeProjectRepository;
+import com.corporatemasters.workinsights.repository.EmployeeRepository;
+import com.corporatemasters.workinsights.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,10 @@ import java.util.List;
 public class EmployeeProjectService {
     @Autowired
     EmployeeProjectRepository employeeProjectRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    ProjectRepository projectRepository;
     public void save(EmployeeProjectDTO employeeProjectDTO) {
         employeeProjectRepository.save(employeeProjectDTO.getEmployeeId(), employeeProjectDTO.getProjectId(), employeeProjectDTO.getStartDate(), employeeProjectDTO.getEndDate());
     }
@@ -18,5 +24,22 @@ public class EmployeeProjectService {
         for (EmployeeProjectDTO employeeProjectDTO : employeeProjectDTOs) {
             employeeProjectRepository.save(employeeProjectDTO.getEmployeeId(), employeeProjectDTO.getProjectId(), employeeProjectDTO.getStartDate(), employeeProjectDTO.getEndDate());
         }
+    }
+    public void saveAllIfNotExist(List<EmployeeProjectDTO> employeeProjectDTOs) {
+        for (EmployeeProjectDTO employeeProjectDTO : employeeProjectDTOs) {
+            if (exists(employeeProjectDTO)) {
+                throw new IllegalArgumentException("One or more records are overlapping!");
+            }
+            if (!employeeRepository.existsById(employeeProjectDTO.getEmployeeId())) {
+                throw new IllegalArgumentException("One or more employee IDs do not exist!");
+            }
+            if (!projectRepository.existsById(employeeProjectDTO.getProjectId())) {
+                throw new IllegalArgumentException("One or more project IDs do not exist!");
+            }
+        }
+        saveAll(employeeProjectDTOs);
+    }
+    public boolean exists(EmployeeProjectDTO employeeProjectDTO) {
+        return employeeProjectRepository.exists(employeeProjectDTO.getEmployeeId(), employeeProjectDTO.getProjectId(), employeeProjectDTO.getStartDate(), employeeProjectDTO.getEndDate());
     }
 }
