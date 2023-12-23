afc8a8ac23f802c32c00fba9820e79f752c6372c
Nikkson <nicky.iliev@abv.bg>
2023-12-23 14:37:18 +0200

    Add getters and setters in EmployeeProject entity

--- src/main/java/com/corporatemasters/workinsights/model/EmployeeProject.java
+++ src/main/java/com/corporatemasters/workinsights/model/EmployeeProject.java
@@ -18,4 +18,44 @@ public class EmployeeProject {
     Project project;
     Date startDate;
     Date endDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
