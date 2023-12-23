51cd9024a8224aa75485535d135b0a088fa3a16c
Nikkson <nicky.iliev@abv.bg>
2023-12-23 14:40:43 +0200

    Add custom query to find all overlapping entries

--- src/main/java/com/corporatemasters/workinsights/repository/EmployeeProjectRepository.java
+++ src/main/java/com/corporatemasters/workinsights/repository/EmployeeProjectRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
+import java.util.List;
 
 
 public interface EmployeeProjectRepository extends JpaRepository<EmployeeProject, Long> {
@@ -17,4 +18,6 @@ public interface EmployeeProjectRepository extends JpaRepository<EmployeeProject
     void save(@Param("employeeId")Long employeeId, @Param("projectId")Long projectId, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
     @Query(value = "select case when count(id)> 0 then 'True' else 'False' end from employee_project ep where employee_id=(:employeeId) and project_id=(:projectId) and start_date < (:endDate) and end_date > (:startDate)", nativeQuery = true)
     boolean exists(@Param("employeeId")Long employeeId, @Param("projectId")Long projectId, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
+    @Query(value = "select * from employee_project ep where employee_id!=(:employeeId) and project_id=(:projectId) and start_date < (:endDate) and end_date > (:startDate)", nativeQuery = true)
+    List<EmployeeProject> findAllOverlapping(@Param("employeeId")Long employeeId, @Param("projectId")Long projectId, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
 }
